a7a9614be8eaffa77bec96152ef0607b5a02865f
Dan Dobrin <dan.dobrin@gmail.com>
2021-07-30 11:20:44 -0400

    first run

---
+++ generate.sh
@@ -0,0 +1,5 @@
+java -jar bin/openapi-generator-cli.jar generate -i openapi/petclinic.yaml -o . -g spring -c ./config.yaml
+
+
+# java -jar openapi-generator-cli.jar generate -i openapi/petclinic.yaml -o . -g spring -c ./config.json
+
